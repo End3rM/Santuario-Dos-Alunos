0995debcd0d44ad9f169dd4028ce268cbaa4d70b
Yasmim Alves <yasmimpython@gmail.com>
2025-09-10 14:24:00 -0300

    Add files via upload

---
+++ Modulo 1/Extra/manipulaçao de arquivo.py
@@ -0,0 +1,26 @@
+# 1. Esta atividade vai trabalhar os comandos Try except e também o comando open()
+# para criação do arquivo o intuito é tentar realizar a leitura do arquivo caso ele não
+# exista você deve tratar este erro criando o arquivo.
+# Passo a passo:
+# Usando os conceitos de Try e Except tente fazer a leitura de um arquivo usando o
+# comando.
+# with open('nome do arquivo', 'modo') as arquivo:
+# variavel = arquivo.modo('conteudo que voce quer inserir')
+# print(variavel)
+# agora é com você , de forma criativa trate o erro usando Except
+# OBS: as palavras variável ,modo e nome do arquivo devem ser substituídas 😉
+
+# 2. Agenda de Contatos
+# Implemente um programa que crie e gerencie um arquivo chamado contatos.txt.
+# Cada contato deve ter nome, telefone e e-mail.
+# O programa deve:
+# Permitir cadastrar novos contatos.
+# Exibir todos os contatos cadastrados.
+# OBS: Neste caso só iremos usar o os comandos de With , open e os modos de leitura
+# e escrita da função open()
+
+# 3 Crie um sistema que regista o nome e 3 notas do aluno depois você vai validar se a média
+# do aluno é maior que 7 caso seja maior você deve registrar em um documento o nome dele
+# e se ele está aprovado ou reprovado.O sistema deve estar em um loop e deve ser
+# encerrado somente se o usuário digitar a opção 0
+
